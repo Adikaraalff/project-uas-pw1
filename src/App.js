1f74c24e76c687f222838a3355760d54704c5bf6
Adikara <73709213+Adikaraalff@users.noreply.github.com>
2022-12-20 21:50:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Col, Container, Row } from 'react-bootstrap';
+import { NavbarComponent, Masukan, ListCategories } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>Daftar Produk</h4>
+            </Col>
+            <Masukan />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Masukan from "./Masukan";
+import {NavbarComponent} from "./NavbarComponent";
+import ListCategories from "./ListCategories"; 
+
+export {Masukan, NavbarComponent, ListCategories}
