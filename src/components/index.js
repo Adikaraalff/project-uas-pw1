137334243631d08614490a0f0d465f9035d2690c
Adikara <73709213+Adikaraalff@users.noreply.github.com>
2022-12-23 19:51:39 +0700

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Masukan from "./Masukan";
-import {NavbarComponent} from "./NavbarComponent";
-import ListCategories from "./ListCategories"; 
-
-export {Masukan, NavbarComponent, ListCategories}
