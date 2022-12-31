db166a3fd1d9bde6df117a2996341459ab58713b
Adikara <73709213+Adikaraalff@users.noreply.github.com>
2022-12-31 15:26:24 +0700

    new commit

---
+++ part2/index.js
@@ -0,0 +1,55 @@
+let vm = new Vue({
+    el: "#app",
+    data: {
+      title: "Data API Pembeli",
+      description: "Data API dideploy di vercel.app",
+      dataPembeli: null,
+  
+      nama: null,
+      alamat: null,
+      nohp: null,
+      alat: null,
+    },
+    methods: {
+      savePembeli: function () {
+        console.log("Button Simpan ditekan");
+        let _data = {
+          nama: this.nama,
+          alamat: this.alamat,
+          nohp: this.nohp,
+          alat: this.alat
+        };
+        axios
+          .post("https://api-new1-adikaraalff.vercel.app/pembeli", _data)
+          .then((response) => {
+            console.log(response);
+            $("#exampleModal").modal("hide");
+            this.getPembeli();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      deletePembeli(_id) {
+        axios
+          .delete("https://api-new1-adikaraalff.vercel.app/pembeli/"+ _id)
+          .then((response) => {
+            this.getPembeli();
+          })
+          .catch(error =>{
+              console.log(error);
+          })
+      },
+      getPembeli() {
+        axios.get("https://api-new1-adikaraalff.vercel.app/pembeli").then((response) => {
+          console.log(response);
+          this.dataPembeli = response.data;
+        });
+      },
+    },
+  
+    mounted() {
+      this.getPembeli();
+    },
+  });
+  
